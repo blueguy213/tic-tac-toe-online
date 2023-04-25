6fcb61eaffe7a1862f28ab7f703bc88509c4cc64
Edward Fominykh <edwardfom@gmail.com>
2023-04-25 18:47:11 -0400

    Added some handling of two clients

--- src/server/ttts.h
+++ src/server/ttts.h
@@ -10,6 +10,10 @@
     #include <sys/types.h>
     #include <sys/socket.h>
     #include <netinet/in.h>
+    #include <sys/select.h>
+    #include <stdbool.h>
+
+    #define BUFFER_SIZE 1024
 
     #define THREAD_POOL_SIZE 10
     #define SERVER_BACKLOG 100
@@ -17,6 +21,5 @@
 
     void* handle_connection(void* p_client_socket);
     int check(int exp, const char* msg);
-
 
 #endif
