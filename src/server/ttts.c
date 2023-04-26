919469c195f51393923dd75f95556fd9041d55f7
Edward Fominykh <edwardfom@gmail.com>
2023-04-25 20:16:41 -0400

    server fix

--- src/server/ttts.c
+++ src/server/ttts.c
@@ -1,73 +1,58 @@
 #include "ttts.h"
+#define MAX_CLIENTS 100
 
 void *client_handler(void *arg);
 int send_to_socket(int socket, const char *str);
 
 int main(int argc, char** argv) {
     
-    // Check command line arguments
     if (argc != 2) {
         printf("Usage: %s <port>\n", argv[0]);
         return 1;
     }
 
-    // Declare local variables
-    int port, server_socket, client_socket, addr_size, player_count = 0;
-    Player* players = malloc(sizeof(Player) * MAX_PLAYER_COUNT);
-    Player* waiting = NULL;
-    // int  client_count = 0;
+    pthread_t tid[MAX_CLIENTS];
 
-    // Convert port number to integer
-    port = check(atoi(argv[1]), "Not a port");
-    check(port > 0, "Invalid port number");
+    int port, server_socket, client_socket, addr_size;
+    int  client_count = 0;
 
-    // Create server socket
+    port = atoi(argv[1]);
     server_socket = check(socket(AF_INET, SOCK_STREAM, 0), "Failed to create socket");
 
-    // Set port number to listen on and protocol family to support
     SA_IN server_addr, client_addr;
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(port);
     
-    // Bind and listen
     check(bind(server_socket, (SA*)&server_addr, sizeof(server_addr)), "Failed to bind");
     check(listen(server_socket, SERVER_BACKLOG), "Failed to listen");
 
     printf("Server is listening on port %d...\n", port);
 
-    while (1) { // Loop forever waiting for connections
+    while (1) {
 
         printf("Waiting for connections...\n");
 
-        // Accept connection
         size_t addr_size = sizeof(SA_IN);
         check(client_socket = accept(server_socket, (SA*)&client_addr, (socklen_t*)&addr_size), "Failed to accept connection");
 
         printf("Client socket: %d\n", client_socket);
         printf("Client connected: %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-        player_count = player_count + 1;
+        client_count = client_count+1;
 
-        // Read the client's message
-        char recvline[MAX_LINE_LEN];
-        memset(recvline, 0, MAX_LINE_LEN);
-        check(read(client_socket, recvline, MAX_LINE_LEN - 1), "Failed to read from socket");
-        printf("Client message: %s\n", recvline);
-
-        // if (player_count % 2 == 0) {
-        //     int *arg = malloc(sizeof(int) * 2);
-        //     arg[0] = client_socket-1;
-        //     arg[1] = client_socket;
-        //     pthread_create(&tid[player_count/2-1], NULL, client_handler, (void *)arg);
-        // }
+        if (client_count % 2 == 0) {
+            int *arg = malloc(sizeof(int) * 2);
+            arg[0] = client_socket-1;
+            arg[1] = client_socket;
+            pthread_create(&tid[client_count/2-1], NULL, client_handler, (void *)arg);
+        }
 
     }
     return 0;
 }
 
-// Check for errors and kill program if found
 int check(int exp, const char* msg) {
     if (exp == SOCKET_ERROR) {
         err_and_kill(msg);
