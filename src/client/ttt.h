ed80cc7dd015d96f02e928cc7a23b22ac8e84723
Sree Kommalapati <sree.n.kommalapati@gmail.com>
2023-04-26 21:03:54 -0400

    board in ttt.h

--- src/client/ttt.h
+++ src/client/ttt.h
@@ -3,9 +3,9 @@
     #define _TTT_H_
 
     void print_board(char* board);
-    void handle_server_message(char *message, int sockfd);
+    void handle_server_message(char *message, int sockfd, char* board);
     int connect_to_server(const char *ip, int port);
-    void* listener(int sockfd);
+    void* listener(int sockfd, char* board);
     void *receive_messages(void *arg);
     void* game_loop(int sockfd);
     
